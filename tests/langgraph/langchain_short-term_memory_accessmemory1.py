2be7229c85c7b4d0b75287af4f10acb9ad3db45a
fa.fu <alanmathisonturing@163.com>
2025-11-25 07:38:01 +0000

    short-term memory

---
+++ tests/langgraph/langchain_short-term_memory_accessmemory1.py
@@ -0,0 +1,34 @@
+from langchain.agents import create_agent, AgentState
+from langchain.tools import tool, ToolRuntime
+import os
+from langchain_openai import ChatOpenAI
+
+model = ChatOpenAI(
+    model="doubao-seed-1-6-251015",  # Specify a model available on OpenRouter
+    api_key=os.environ.get("ARK_API_KEY"),
+    base_url="https://ark.cn-beijing.volces.com/api/v3",
+)
+
+class CustomState(AgentState):
+    user_id: str
+
+@tool
+def get_user_info(
+    runtime: ToolRuntime
+) -> str:
+    """Look up user info."""
+    user_id = runtime.state["user_id"]
+    return "User is John Smith" if user_id == "user_123" else "Unknown user"
+
+agent = create_agent(
+    model=model,
+    tools=[get_user_info],
+    state_schema=CustomState,
+)
+
+result = agent.invoke({
+    "messages": "look up user information",
+    "user_id": "user_123"
+})
+print(result["messages"][-1].content)
+# > User is John Smith.
---
+++ tests/langgraph/langchain_short-term_memory_prompt.py
@@ -0,0 +1,43 @@
+from langchain.agents import create_agent
+from typing import TypedDict
+from langchain.agents.middleware import dynamic_prompt, ModelRequest
+import os
+from langchain_openai import ChatOpenAI
+from langchain.tools import tool
+
+model = ChatOpenAI(
+    model="doubao-seed-1-6-251015",  # Specify a model available on OpenRouter
+    api_key=os.environ.get("ARK_API_KEY"),
+    base_url="https://ark.cn-beijing.volces.com/api/v3",
+)
+
+class CustomContext(TypedDict):
+    user_name: str
+
+
+@tool
+def get_weather(city: str) -> str:
+    """Get the weather in a city."""
+    return f"The weather in {city} is always sunny!"
+
+
+@dynamic_prompt
+def dynamic_system_prompt(request: ModelRequest) -> str:
+    user_name = request.runtime.context["user_name"]
+    system_prompt = f"You are a helpful assistant. Address the user as {user_name}."
+    return system_prompt
+
+
+agent = create_agent(
+    model=model,
+    tools=[get_weather],
+    middleware=[dynamic_system_prompt],
+    context_schema=CustomContext,
+)
+
+result = agent.invoke(
+    {"messages": [{"role": "user", "content": "What is the weather in SF?"}]},
+    context=CustomContext(user_name="John Smith"),
+)
+for msg in result["messages"]:
+    msg.pretty_print()

c922bbe5701970ce725f76a2b037b1c863d96321
fa.fu <alanmathisonturing@163.com>
2025-11-25 05:34:28 +0000

    model structedoutput

---
+++ tests/langgraph/langchain_model_structedoutput.py
@@ -0,0 +1,47 @@
+from pydantic import BaseModel, Field
+import os
+from langchain_openai import ChatOpenAI
+
+model = ChatOpenAI(
+    model="doubao-seed-1-6-251015",  # Specify a model available on OpenRouter
+    api_key=os.environ.get("ARK_API_KEY"),
+    base_url="https://ark.cn-beijing.volces.com/api/v3",
+)
+
+
+class Movie(BaseModel):
+    """A movie with details."""
+    title: str = Field(..., description="The title of the movie")
+    year: int = Field(..., description="The year the movie was released")
+    director: str = Field(..., description="The director of the movie")
+    rating: float = Field(..., description="The movie's rating out of 10")
+
+model_with_structure = model.with_structured_output(Movie)
+response = model_with_structure.invoke("Provide details about the movie Inception")
+print(response)  # Movie(title="Inception", year=2010, director="Christopher Nolan", rating=8.8)
+
+
+model_with_structure = model.with_structured_output(Movie, include_raw=True)  
+response = model_with_structure.invoke("Provide details about the movie Inception")
+print(response)
+# {
+#     "raw": AIMessage(...),
+#     "parsed": Movie(title=..., year=..., ...),
+#     "parsing_error": None,
+# }
+
+
+class Actor(BaseModel):
+    name: str
+    role: str
+
+class MovieDetails(BaseModel):
+    title: str
+    year: int
+    cast: list[Actor]
+    genres: list[str]
+    budget: float | None = Field(None, description="Budget in millions USD")
+
+model_with_structure = model.with_structured_output(MovieDetails)
+response = model_with_structure.invoke("Provide details about the movie Inception")
+print(response)

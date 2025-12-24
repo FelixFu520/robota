cb6f441a37a487efc4dbfb5a1a2a0764bf9b8a45
fa.fu <alanmathisonturing@163.com>
2025-12-24 14:08:51 +0800

    test doubao llm

--- robota/model/__init__.py
+++ robota/model/__init__.py
@@ -1,4 +1,4 @@
-from robota.models.doubao import DOUBAO_SEED_1_6_251015, DOUBAO_SEED_1_6_LITE_251015
+from robota.model.doubao import DOUBAO_SEED_1_6_251015, DOUBAO_SEED_1_6_LITE_251015
 
 __all__ = [
     'DOUBAO_SEED_1_6_251015',
---
+++ tests/llm/openai_doubao_stream.py
@@ -0,0 +1,20 @@
+import os
+from robota.model.doubao import DOUBAO_SEED_1_6_251015 as model
+
+print('*'*100)
+for chunk in model.stream("介绍一下你自己"):
+    print(chunk.text, end="|", flush=True)
+print('*'*100)
+
+print('='*100)
+for chunk in model.stream("介绍一下你自己"):
+    for block in chunk.content_blocks:
+        if block["type"] == "reasoning" and (reasoning := block.get("reasoning")):
+            print(f"Reasoning: {reasoning}")
+        elif block["type"] == "tool_call_chunk":
+            print(f"Tool call chunk: {block}")
+        elif block["type"] == "text":
+            print(block["text"])
+        else:
+            pass
+print('='*100)

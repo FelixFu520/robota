9ea65b49966849b84edd93e316121563394bd6a0
fa.fu <alanmathisonturing@163.com>
2025-12-24 17:17:11 +0800

    关闭思考

--- tests/llm/openai_doubao_stream.py
+++ tests/llm/openai_doubao_stream.py
@@ -2,19 +2,25 @@ import os
 from robota.model.doubao import DOUBAO_SEED_1_6_251015 as model
 
 print('*'*100)
-for chunk in model.stream("介绍一下你自己"):
+for chunk in model.stream("介绍一下你自己", extra_body={
+        "thinking": {
+            "type": "disabled"  # 不使用深度思考能力
+            # "type": "enabled" # 使用深度思考能力
+            # "type": "auto" # 模型自行判断是否使用深度思考能力
+        }
+    }):
     print(chunk.text, end="|", flush=True)
 print('*'*100)
 
-print('='*100)
-for chunk in model.stream("介绍一下你自己"):
-    for block in chunk.content_blocks:
-        if block["type"] == "reasoning" and (reasoning := block.get("reasoning")):
-            print(f"Reasoning: {reasoning}")
-        elif block["type"] == "tool_call_chunk":
-            print(f"Tool call chunk: {block}")
-        elif block["type"] == "text":
-            print(block["text"])
-        else:
-            pass
-print('='*100)
+# print('='*100)
+# for chunk in model.stream("介绍一下你自己"):
+#     for block in chunk.content_blocks:
+#         if block["type"] == "reasoning" and (reasoning := block.get("reasoning")):
+#             print(f"Reasoning: {reasoning}")
+#         elif block["type"] == "tool_call_chunk":
+#             print(f"Tool call chunk: {block}")
+#         elif block["type"] == "text":
+#             print(block["text"])
+#         else:
+#             pass
+# print('='*100)

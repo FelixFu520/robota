1bee50c8033ee1d8893639cc085c1dbb4d1bdc71
fa.fu <alanmathisonturing@163.com>
2025-12-29 15:07:27 +0800

    add asrtts

--- robota/audio/__init__.py
+++ robota/audio/__init__.py
@@ -4,6 +4,8 @@ from robota.audio.volcengine_doubao_tts import (
     start_session, finish_session, cancel_session, task_request,
     receive_message, wait_for_event, start_connection, finish_connection,
 )
+from robota.audio.asr_tts import ASRTTS
+
 __all__ = [
     'AudioDevice',
     'AsrWsClient',
@@ -15,4 +17,5 @@ __all__ = [
     'wait_for_event',
     'start_connection',
     'finish_connection',
+    'ASRTTS',
 ]
